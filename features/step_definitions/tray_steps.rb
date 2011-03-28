aca858e2ca2b207855032753aacc60f38cea7e28
Julian Bäume <julian@svg4all.de>
2011-03-28 02:55:28 +0200

    add feature definition for the tray
    it contains a first scenario, but something seems broken with recent
    rails. for some reasons the user model is not updated or updated wrong.
    this is not a bug in the TrayPosition class, but somewhere in Asset,
    acts_as_asset or User, not sure, what exactly goes wrong here.
    
    comiting anyway, to have these tests in the repo

---
+++ features/step_definitions/tray_steps.rb
@@ -0,0 +1,26 @@
+Given /^I am logged in as user "([^"]*)"$/ do |login|
+  @user = User.find_by_login(login)
+  @user.should_not be_nil
+end
+
+Given /^this user has no assets on the tray$/ do
+  @user.tray_positions.should == []
+end
+
+When /^I add the following items to the tray:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  table.hashes.each do |hash|
+    a = hash[:type].constantize.new({:asset_attributes => {:name => hash[:name]}})
+    a.asset.user = @user
+    a.save!
+  end
+end
+
+Then /^I should see these (\d+) items named:$/ do |count, table|
+  # table is a Cucumber::Ast::Table
+  @user.tray_positions.size.should == count.to_i
+
+  table.hashes.each do |hash|
+    @user.tray_positions.should contain(hash[:name])
+  end
+end
