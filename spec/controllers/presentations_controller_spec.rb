778d6f73316e2aaf629cafeaabfc946a4d259f24
Julian Bäume <julian@svg4all.de>
2011-07-12 19:35:17 +0200

    add spec for presentations controller new action
    There is a bug, that the PresentationsController (or in this case the
    AssetsController) creates new assets of the wrong class.
    
    BUG: [#1909] -
    https://dev.imis.uni-luebeck.de/projects/fassets/tickets/1909

---
+++ spec/controllers/presentations_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe PresentationsController do
+  include Devise::TestHelpers
+
+  def mock_user(stubs={})
+    @mock_user ||= mock_model(User, stubs).as_null_object
+  end
+
+  before(:each) do
+    # mock up an authentication in the underlying warden library
+    request.env['warden'] = mock(Warden, :authenticate => mock_user,
+                                 :authenticate! => mock_user)
+  end
+
+  describe "GET 'new'" do
+    it "should be successful and render all partials" do
+      get 'new'
+      response.should be_success
+      response.should render_template("assets/new")
+      assigns(:content).class.should == "Presentation"
+    end
+  end
+end
