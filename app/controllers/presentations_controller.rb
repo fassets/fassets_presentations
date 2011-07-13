941b7ccfd60f56c46d5324266413a9e2f00bd8d5
Christopher Sharp <cdsharp@gmail.com>
2011-07-13 19:23:10 +0200

    Fix a change in presentations_controller i forgot to merge

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -11,14 +11,4 @@ class PresentationsController < AssetsController
   def content_model
     return Presentation
   end
-  def create
-    @content = Presentation.new(params['presentation'])
-    @content.asset = Asset.create(:user => current_user, :name => params["asset"]["name"])
-    if @content.save
-      flash[:notice] = "Created new asset!"
-      redirect_to url_for(@content) + "/edit"
-    else
-      render :action => 'new'
-    end
-  end
 end
