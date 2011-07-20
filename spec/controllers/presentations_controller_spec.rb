5cbda97524f13c2843fd6fd831a2cd71a3275258
Julian Bäume <julian@svg4all.de>
2011-07-20 13:16:05 +0200

    move include call for devise test helpers
    it’s now in a more central place and mustn’t be included for every new
    controller to spec

--- spec/controllers/presentations_controller_spec.rb
+++ spec/controllers/presentations_controller_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
 describe PresentationsController do
-  include Devise::TestHelpers
-
   def mock_user(stubs={})
     @mock_user ||= mock_model(User, stubs).as_null_object
   end
