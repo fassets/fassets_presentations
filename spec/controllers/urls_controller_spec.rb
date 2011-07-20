f4fcd50123704f8ee4e22c802a225ade32e0e36f
Julian Bäume <julian@svg4all.de>
2011-07-20 14:05:18 +0200

    add first specs for urls_controller

---
+++ spec/controllers/urls_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe UrlsController do
+  describe "GET 'show'" do
+    let(:url) { double(Url, :id => 1, :url => "http://example.com/") }
+
+    context "asset doesn’t exist" do
+      it "should redirect to application root" do
+        get 'show', :id => url.id
+        response.should redirect_to(root_path)
+      end
+
+      it "should flash out an error" do
+        pending "implement me!"
+        get 'show', :id => url.id
+        response.should redirect_to(root_path)
+        request.flash[:error].should =~ /not found$/
+      end
+    end
+
+    context "asset exists" do
+      before(:each) do
+        my_url = url
+        controller.stub!(:find_content) {}
+        controller.instance_eval { @content = my_url }
+      end
+
+      it "should redirect to url" do
+        get 'show', :id => url.id
+        response.should redirect_to(url.url)
+      end
+    end
+  end
+end
