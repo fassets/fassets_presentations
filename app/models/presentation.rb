e7ee3fce45414ad97d43e06604dad05b73a97fcc
Julian Bäume <julian@svg4all.de>
2011-08-19 19:20:48 +0200

    add root_frame accessor to presentation
    the root frame should not be considered by any view, except the list of
    possible parents for new frames.
    
    unfortunately, for now, the frames association also contains the root
    frame. this needs to be considered when working with a presentation’s
    frames. (e.g. presentation.frames.count is to large, also while
    iterating, one must check for the root frame)

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -3,14 +3,27 @@ class Presentation < ActiveRecord::Base
   acts_as_asset
   after_create :create_root_frame
   has_many :frames, :order => :position, :dependent => :destroy
+  has_one :root_frame, :class_name => 'Frame', :readonly => true, :dependent => :destroy
 
   validates_presence_of :title,:template
-  
+
+  def root_frame=(value)
+    raise "can’t overwrite root frame"
+  end
+
   def media_type
     "Presentation"
   end
+
+  protected
   def create_root_frame
-    root_frame = Frame.new(:title => "root_frame", :template => "title", :presentation_id => id, :position => 1)
-    root_frame.save
+    Frame.create!(:title => root_frame_title, :template => "title", :presentation_id => id, :position => 1)
+  end
+
+  def root_frame_title
+    # this is a user-visible string, we might want to translate it one day
+    # since it’s in the parent selection for frames, it might be called "none" to indicate a no parent situation
+    # also don’t forget to write a migraton to change the titles for existing presentations
+    "root_frame"
   end
 end
