94b19a75d3fde779f74a177beb4bebe06dece5c3
Julian Bäume <julian@svg4all.de>
2011-08-20 19:54:25 +0200

    add some more specs for frames controller
    working towards a complete test coverage! ;)

--- spec/controllers/frames_controller_spec.rb
+++ spec/controllers/frames_controller_spec.rb
@@ -14,4 +14,53 @@ describe FramesController do
       response.should redirect_to presentation_path(1)+"#1"
     end
   end
+
+  describe "POST 'create'" do
+    let(:params) do
+      {:presentation_id => 1, :frame => {}}
+    end
+
+    describe "creation is a success" do
+      it "assigns a presentation" do
+        post 'create', params
+        assigns(:presentation).should_not be_nil
+      end
+
+      it "creates a frame" do
+        frame_count = Presentation.find(1).frames.count
+        post 'create', params.merge({:frame => {:title => "TestTitle", :template => 'none'}})
+        request.flash[:notice].should =~ /Frame succesfully created!/
+        Presentation.find(1).frames.count.should == frame_count+1
+      end
+    end
+
+    describe "creation fails" do
+      it "creates no frame" do
+        frame_count = Presentation.find(1).frames.count
+        post 'create', params
+        request.flash[:error].should_not be_empty
+        Presentation.find(1).frames.count.should == frame_count
+      end
+    end
+  end
+
+  describe "PUT 'update'" do
+    let(:params) do
+      {:presentation_id => 1, :id => 2, :frame => {}}
+    end
+
+    it "finds presentation and frame" do
+      put 'update', params
+      assigns(:presentation).should_not be_nil
+      assigns(:frame).should_not be_nil
+    end
+
+    describe "update successful" do
+      it "should update the frame" do
+        put 'update', params.merge({:frame => {:title => "changed"}})
+        assigns(:frame).title.should == "changed"
+        request.flash[:notice].should =~ /succesfully updated/
+      end
+    end
+  end
 end
