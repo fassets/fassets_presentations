171bfdf6c3b504bcbcfd9a5605d7beecd8026eed
Julian Bäume <julian@svg4all.de>
2011-09-12 02:10:33 +0200

    remove csrf code and replace by rails methods
    no need to have much code to protect against csrf. better let rails
    handele it. still needs checks, but well. seems to work, for now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,17 +12,6 @@
 //= require_tree .
 //
 $(document).ready(function(){
-  // UJS authenticity token fix: add the authenticy_token parameter
-  // expected by any Rails POST request.
-  $(document).ajaxSend(function(event, request, settings) {
-    // do nothing if this is a GET request. Rails doesn't need
-    // the authenticity token, and IE converts the request method
-    // to POST, just because - with love from redmond.
-    if (settings.type == 'GET') return;
-    if (typeof(AUTH_TOKEN) == "undefined") return;
-    settings.data = settings.data || "";
-    settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
-  })
 	$('.sortable_frames').nestedSortable({
 		disableNesting: 'no-nest',
 		forcePlaceholderSize: true,
