27543312826ae89601a23e3c1cb71302c92b1dee
Julian Bäume <julian@svg4all.de>
2011-09-21 13:29:44 +0200

    fix indentation and white-space

--- templates/default/public/script.js
+++ templates/default/public/script.js
@@ -1,73 +1,68 @@
 var Slide = {
-	width: function() {
-		return $(window).width();
-	},
-	height: function() {
-		return $(window).height();
-	},
-	syncSize: function() {
-		var size = Frame.height() / 30;
-	    $("body").css("font-size", size + 'px');
-	    $(".frame").css("width", Frame.width() + 'px');
-	    $(".frame").css("height", Frame.height() + 'px');
+  width: function() {
+    return $(window).width();
+  },
+  height: function() {
+    return $(window).height();
+  },
+  syncSize: function() {
+    var size = Frame.height() / 30;
+    $("body").css("font-size", size + 'px');
+    $(".frame").css("width", Frame.width() + 'px');
+    $(".frame").css("height", Frame.height() + 'px');
 
-		/*
-		$(".fit").each(function() {
-			if (!$(this).data('dimensions')) {
-				!$(this).data('dimensions', {
-					width: $(this).width(), 
-					height: $(this).height(),
-					parent: $(this).parent()
-				});
-			}
-			var width = $(this).data('dimensions').width;
-			var height = $(this).data('dimensions').height;
+//     $(".fit").each(function() {
+//       if (!$(this).data('dimensions')) {
+//         $(this).data('dimensions', {
+//           width: $(this).width(),
+//                      height: $(this).height(),
+//                      parent: $(this).parent()
+//         });
+//       }
+//       var width = $(this).data('dimensions').width;
+//       var height = $(this).data('dimensions').height;
+//
+//       var ratio = width/height;
+//       var parent = $(this).data('dimensions').parent;
+//       var new_width = 0;
+//       var new_height = 0;
+//
+//       if (width > height) {
+//         if (parent.width() > parent.height()) {
+//           new_height = parent.height();
+//           new_width = Math.round(new_height * ratio);
+//         } else {
+//           new_width = parent.width();
+//           new_height = new_width / ratio;
+//         }
+//       } else {
+//         if (parent.width() > parent.height()) {
+//           new_height = parent.height();
+//           new_width = new_height / ratio;
+//         } else {
+//           new_width = parent.width();
+//           new_height = new_width * ratio;
+//         }
+//       }
+//       $(this).width(new_width);
+//       $(this).height(new_height);
+//       console.log("" + new_width + "x" + new_height + " parent: " + parent.width() + "x" + parent.height() + " parent_id: " + parent.attr("id"));
+//     });
 
-			
-			var ratio = width/height;
-			var parent = $(this).data('dimensions').parent;
-			var new_width = 0;
-			var new_height = 0;
-
-
-			if (width > height) {
-				if (parent.width() > parent.height()) {
-					new_height = parent.height();
-					new_width = Math.round(new_height * ratio);
-
-				} else {
-					new_width = parent.width();
-					new_height = new_width / ratio;					
-				}
-			} else {
-				if (parent.width() > parent.height()) {
-					new_height = parent.height();
-					new_width = new_height / ratio;
-				} else {
-					new_width = parent.width();
-					new_height = new_width * ratio;					
-				}
-			}
-			$(this).width(new_width);
-			$(this).height(new_height);
-			console.log("" + new_width + "x" + new_height + " parent: " + parent.width() + "x" + parent.height() + " parent_id: " + parent.attr("id"));
-		});*/
-		
-		//Align vertically centered elements
-		$(".center_vertically").each(function() {
-			var pos = ($(this).parent().height() - $(this).height()) / 2;
-			$(this).css("position","absolute");
-			$(this).css("top", pos + "px");
-		});
-	}
+    //Align vertically centered elements
+    $(".center_vertically").each(function() {
+      var pos = ($(this).parent().height() - $(this).height()) / 2;
+      $(this).css("top", pos + "px");
+    });
+  }
 }
 
 $(window).ready(function(){
-	Frame.syncSize();
+  Frame.syncSize();
 });
 $(window).resize(function(){
-	console.log("resize event")
-	Frame.syncSize();
+  console.log("resize event")
+  Frame.syncSize();
 });
 
 functian toggleNavigationMenu() {
@@ -81,12 +76,12 @@ functian toggleNavigationMenu() {
 }
 
 /*$(window).ready(function(){
-	Slide.syncSize();
+  Slide.syncSize();
 });
 $(window).resize(function(){
-	Slide.syncSize();
+  Slide.syncSize();
 });
 
 $("#content").click(function(){
-	alert("this.width()");
+  alert("this.width()");
 })*/
