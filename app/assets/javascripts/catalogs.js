dc53921698b5bf8ad7b9ba370e35964200d5c7d9
Julian Bäume <julian@svg4all.de>
2011-10-07 17:27:54 +0200

    fix indentation

--- app/assets/javascripts/catalogs.js
+++ app/assets/javascripts/catalogs.js
@@ -1,13 +1,13 @@
 $(function(){
-	$("#assets li.asset").draggable({
-		handle: ".handle",
-		helper: "clone",
-		connectToSortable: "#tray ol",
-		start:function(e, ui) {
-			$('#tray ol').addClass("active");
-		}, 
-		stop:function(e, ui) {
-			$('#tray ol').removeClass("active");
-		}
-	});
+  $("#assets li.asset").draggable({
+    handle: ".handle",
+    helper: "clone",
+    connectToSortable: "#tray ol",
+    start:function(e, ui) {
+      $('#tray ol').addClass("active");
+    },
+    stop:function(e, ui) {
+      $('#tray ol').removeClass("active");
+    }
+  });
 });
