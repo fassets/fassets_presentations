2eddba2f520999c03272511f3c39664637042385
Christopher Sharp <cdsharp@gmail.com>
2011-10-29 13:32:19 +0200

    Fix problems on the edit page of presentations. - Render the forms - Make all buttons work (Delete,Save,Make Copy)

--- app/controllers/fassets-presentations/presentations_controller.rb
+++ app/controllers/fassets-presentations/presentations_controller.rb
@@ -2,37 +2,48 @@ module FassetsPresentations
   class PresentationsController < ApplicationController
     before_filter :find_content, :except => [:new, :create, :preview, :markup_preview,:copy]
     def new
-      logger.debug("PresentationsController of FassetsPresentations")
       @asset = Fassets::Application::Asset.new
-      logger.debug(Fassets::Application::Asset.new)
-      @presentation = FassetsPresentations::Presentation.new
+      @presentation = Presentation.new
       @content = @presentation
       render :template => "fassets-presentations/presentations/new"
     end
     def create
-      @content = FassetsPresentations::Presentation.new(params[:presentation])
+      @content = Presentation.new(params[:presentation])
       @content.asset = Fassets::Application::Asset.create(:user => current_user, :name => params["asset"]["name"])
       if @content.save
         flash[:notice] = "Created new asset!"
-        redirect_to url_for(@content) + "/edit"
+        redirect_to fassets_presentations.url_for(@content) + "/edit"
       else
         render :template => "fassets-presentations/presentations/new"
       end
+    end
+    def destroy
+      flash[:notice] = "Asset has been deleted!"
+      @content.destroy
+      redirect_to main_app.root_url
     end
     def edit
       render :template => "fassets-presentations/presentations/edit"
     end
     def show
-      logger.debug("FassetsPresentations - Show")
       @presentation = @content
       render :template => "fassets-presentations/presentations/show", :layout => "fassets-presentations/layouts/frame"
     end
+    def update
+      if @content.update_attributes(params[:presentation]) and @content.asset.update_attributes(params["asset"])
+        flash[:notice] = "Succesfully updated asset!"
+        redirect_to fassets_presentations.url_for(@content) + "/edit"
+      else
+        flash[:error] = "Could not update asset!"
+        render :template => 'fassets-presentations/presentations/edit'
+      end
+    end
     def content_model
-      return FassetsPresentations::Presentation
+      return Presentation
     end
     def copy
-      old_presentation = FassetsPresentations::Presentation.find(params[:presentation_id])
-      new_presentation = FassetsPresentations::Presentation.create(:title => old_presentation.title, :template => old_presentation.template)
+      old_presentation = Presentation.find(params[:presentation_id])
+      new_presentation = Presentation.create(:title => old_presentation.title, :template => old_presentation.template)
       new_presentation.asset = old_presentation.asset.clone
       new_presentation.asset.name = old_presentation.asset.name + " - Copy"
       new_presentation.asset.classifications = old_presentation.asset.classifications
@@ -40,7 +51,7 @@ module FassetsPresentations
       copy_frames(new_presentation.root_frame, old_presentation.root_frame.children)
       @presentation = new_presentation
       @content = new_presentation
-      redirect_to url_for(@content) + "/edit"
+      redirect_to fassets_presentations.url_for(@content) + "/edit"
     end
     def copy_frames(new_parent,frames)
       unless frames.length == 0
@@ -60,7 +71,6 @@ protected
       else
         content_id = params[:id]
       end
-      logger.debug(content_id)
       @content = Presentation.find(content_id)
     end
   end
