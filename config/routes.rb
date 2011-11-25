fa5186260c43d752395fba531f3bad1ca7992546
Julian Bäume <julian@svg4all.de>
2011-11-25 04:39:15 +0100

    additions fo cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ FassetsPresentations::Engine.routes.draw do
   put '/:id' => 'Presentations#update', :as => 'update_presentation'
 
   get '/:presentation_id/frames' => 'Frames#index', :as => 'presentation_frames'
-  get '/:presentation_id/frame/:id' => 'Frames#show', :as => 'presentation_frame'
+  post '/:presentation_id/frames' => 'Frames#create', :as => 'presentation_frames'
   get '/:presentation_id/frame/:id/edit' => 'Frames#edit', :as => 'edit_presentation_frame'
+  get '/:presentation_id/frame/:id' => 'Frames#show', :as => 'presentation_frame'
   put '/:presentation_id/frame/:id' => 'Frames#update', :as => 'update_presentation_frame'
+  delete '/:presentation_id/frame/:id' => 'Frames#destroy', :as => 'delete_presentation_frame'
   post '/:presentation_id/frames/sort' => 'Frames#sort', :as => 'sort_presentation_frames'
 
   post :copy, :as => 'presentation_copy'
