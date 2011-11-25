b5cc597360fefadb31f9a47704a5bf96ec19e029
Julian Bäume <julian@svg4all.de>
2011-11-25 03:19:22 +0100

    cleanup routes
    since it’s not easy to use resources directly in the engine, we have to
    lay out the routes manually. This adds the missing routes that are
    needed for the engine. This should simplify the code in views, helpers
    and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,11 @@ FassetsPresentations::Engine.routes.draw do
   get '/new' => 'Presentations#new', :as => 'new_presentation'
   put '/:id' => 'Presentations#update', :as => 'update_presentation'
 
-  resources :presentations do
-    resources :frames do
-     collection do
-        post :sort
-      end
-    end
-  end
+  get '/:presentation_id/frames' => 'Frames#index', :as => 'presentation_frames'
+  get '/:presentation_id/frame/:id' => 'Frames#show', :as => 'presentation_frame'
+  get '/:presentation_id/frame/:id/edit' => 'Frames#edit', :as => 'edit_presentation_frame'
+  put '/:presentation_id/frame/:id' => 'Frames#update', :as => 'update_presentation_frame'
+  post '/:presentation_id/frames/sort' => 'Frames#sort', :as => 'sort_presentation_frames'
 
   post :copy, :as => 'presentation_copy'
   match 'markup/preview' => 'frames#markup_preview'
