168acb23f235f2987c6b531ba9e7cff50a17b621
Julian Bäume <julian@svg4all.de>
2012-06-26 00:26:13 +0200

    add jquery.qtip plugin
    Since there is no rails plugin shipping this code, we ship it with the
    application. In the long-term, this should be moved out of the tree or
    be removed in favour of another gem.

--- app/assets/javascripts/fassets_presentations/application.js
+++ app/assets/javascripts/fassets_presentations/application.js
@@ -10,3 +10,4 @@
 //= require fassets_presentations/frames_wysiwyg
 //= require fassets_presentations/sortable_frames
 //= require fassets_presentations/pres_edit_box
+//= require jquery.qtip-1.0.0-rc3
