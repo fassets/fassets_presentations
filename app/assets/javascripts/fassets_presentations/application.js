ccc6a7bcc0e891b32db85c48d5e4f4145a64ef32
Julian Bäume <julian@svg4all.de>
2012-06-25 14:01:46 +0200

    remove .erb extension for application.js
    There’s no ruby code in there, only references to other js-files.

--- app/assets/javascripts/fassets_presentations/application.js
+++ app/assets/javascripts/fassets_presentations/application.js
@@ -6,6 +6,7 @@
 //
 //= require fassets_presentations/jquery.ui.nestedSortable
 //= require fassets_presentations/jquery.myimgscale-0.2
-// require fassets_presentations/frames
-// require fassets_presentations/frames_wysiwyg
+//= require fassets_presentations/frames
+//= require fassets_presentations/frames_wysiwyg
+//= require fassets_presentations/sortable_frames
 //= require fassets_presentations/pres_edit_box
